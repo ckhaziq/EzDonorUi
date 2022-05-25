ee317a366bf3e1983f4d3e3b30638fcb8ebdfae3
ckhaziq <che.haziq98@gmail.com>
2022-05-26 00:38:37 +0800

    1

--- js/custom.js
+++ js/custom.js
@@ -20,9 +20,10 @@
 
     // BACKSTRETCH SLIDESHOW
     $('#section_1').backstretch([
-      "images/slide/microsoft-edge-FAaz8lkinzs-unsplash.jpg", 
+      "images/slide/9607102311_f0f6370107_o.jpg", 
+      "images/slide/food-donations-box-isolated-white-background-31427220.jpg",
       "images/slide/surface-1x5jnhtlp3Y-unsplash.jpg",
-      "images/slide/surface-71_s6RDJpGc-unsplash.jpg"
+      "images/slide/volunteer-work-young-happy-volunteers-medical-masks-holding-food-donations-box-woman-showing-thumb-up-copy-space-package-236228966.jpg"
     ],  {duration: 2000, fade: 750});
     
   })(window.jQuery);
